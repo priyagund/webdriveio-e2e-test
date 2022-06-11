820a98bab2b0b12c42ed9821bf806790db0d3b8b
Priyanka Gund <priyanka.gund@thoughtworks.com>
2022-06-11 17:20:18 +0530

    "updated dunzo login test and add item to cart"

--- test/specs/dunzo-login.js
+++ test/specs/dunzo-login.js
@@ -5,12 +5,16 @@ describe('Dunzo Login', () => {
         await DunzoLoginPage.open()
     })
 
-    it('search for groceries', async () => {
+    it('search for groceries and add item to cart', async () => {
         await browser.url('https://www.dunzo.com/search')
         await $('input[placeholder="Search for item or a store"]').waitForDisplayed()
         await $('input[placeholder="Search for item or a store"]').setValue('fruits')
         browser.pause(5000)
         await browser.keys("Enter")
-        awiat
+        links = $$('a[href="/product/banana-robusta-12-pcs?dzid=5234ef53-f010-4322-8cd0-498306008b42"]').click()
+        browser.pause(5000)
+        await $(`button*=${"ADD"}`).click()
+        //await $(`button=${"Add item"}`).click()
     })
+
 })
